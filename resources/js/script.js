f89b273a9a46b4af9af3af5e51432867c1861229
uday <udaykumar1113@gmail.com>
2017-12-23 15:56:14 -0500

    Added waypoint for sticky indexing

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,16 @@
-$(document).ready(function(){
-    $('h1').click(function(){
-        $(this).css('background-color','#ff0000');
-    });
+
+$(document).ready(function()
+{
+    $('.js--section-features').waypoint(function(direction)
+    {
+        if(direction="down")
+        {
+            $('nav').addClass('sticky');
+        }                          
+        else
+        {
+            $('nav').removeClass('sticky');
+        }
+});
+
 });
